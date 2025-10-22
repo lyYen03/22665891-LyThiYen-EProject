99dcb2baa21ebe0122abb21ed8c0a1f87e303a6f
ltyen <yenluna60@gmail.com>
2025-10-22 23:19:11 +0700

    fix: convert config to ESM default export

--- auth/src/config/index.js
+++ auth/src/config/index.js
@@ -1,6 +1,9 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-module.exports = {
+const config = {
     mongoURI: process.env.MONGODB_AUTH_URI || "mongodb://127.0.0.1:27017/auth_db",
     jwtSecret: process.env.JWT_SECRET || "mysecretkey",
 };
+
+export default config;
