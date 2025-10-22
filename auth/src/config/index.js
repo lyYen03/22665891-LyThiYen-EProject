466637bfe4b75f440f748eba99a831c6226f7d9c
ltyen <yenluna60@gmail.com>
2025-10-22 17:31:38 +0700

    Fix config fallback for MongoDB URI

--- auth/src/config/index.js
+++ auth/src/config/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  mongoURI: process.env.MONGODB_AUTH_URI,
-  jwtSecret: process.env.JWT_SECRET || "secret",
+    mongoURI: process.env.MONGODB_AUTH_URI || "mongodb://127.0.0.1:27017/auth_db",
+    jwtSecret: process.env.JWT_SECRET || "mysecretkey",
 };
