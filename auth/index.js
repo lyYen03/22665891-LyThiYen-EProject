d7940cb7d7cc07516c0495a90ffa21ae0dbb5862
ltyen <yenluna60@gmail.com>
2025-10-22 23:15:49 +0700

    fix: convert main entry to ESM syntax

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,7 @@
-require("dotenv").config();
-const App = require("./src/app");
+import dotenv from "dotenv";
+import App from "./src/app.js";
+
+dotenv.config();
 
 const app = new App();
 app.start();
